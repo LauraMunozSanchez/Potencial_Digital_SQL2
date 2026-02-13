1d2fa8fdfe8e49f19b890dbb61e57f499283b074
LauraMunozSanchez <lmunozsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-02-13 12:58:16 +0100

    tilde

--- TAR-05.02 Pruebas con FOREIGN KEY (Nuevas tablas en Potencial Digital)_LauraMuñozSanchez/Potencial_Digital_SQL_InsertarFilas.sql
+++ TAR-05.02 Pruebas con FOREIGN KEY (Nuevas tablas en Potencial Digital)_LauraMuñozSanchez/Potencial_Digital_SQL_InsertarFilas.sql
@@ -2,7 +2,7 @@
 INSERT INTO colegios (idColegio, nombre, persona_contacto, tfno_contacto)VALUES
 (1, 'Colegio Norte', 'Ana López', '600111222'),
 (2, 'Colegio Sur', 'Luis Pérez', '600333444'),
-(3, 'Colegio Este', 'María García', '600555666');
+(3, 'Colegio Este', 'María Garcia', '600555666');
 
 -- Insertarmos filas en la tabla 'colegios_actividades'
 INSERT INTO colegios_actividades (idColegio, idCharla, fecha, numero_asistentes) VALUES 
